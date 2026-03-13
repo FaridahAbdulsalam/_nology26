fedeff677a02671adb314f6990e3eda2704a3c7c
Faridah Salami <f.salami@outlook.com>
2026-03-13 12:11:43 +0000

    feat: implement blog previews in Home component with associated styles

--- ReactModule/Udemy-Course/Ninja-tutorial/dojo-blog/src/pages/Home/Home.jsx
+++ ReactModule/Udemy-Course/Ninja-tutorial/dojo-blog/src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { useState } from "react";
 
 const Home = () => {
+
+const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'Anike', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Anix', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Nixxy', id: 3 }
+]);
+ 
   return (
     <>
       <div className="home">
-        <h1>Homepage</h1>
+        {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+            </div>
+        ))}
       </div>
     </>
   );
