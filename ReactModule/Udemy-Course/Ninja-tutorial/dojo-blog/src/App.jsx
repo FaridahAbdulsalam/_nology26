ce97b11ff191f053208f508b9fac96ce85173ffd
Faridah Salami <f.salami@outlook.com>
2026-03-13 10:49:21 +0000

    feat: add App component and associated CSS for styling

---
+++ ReactModule/Udemy-Course/Ninja-tutorial/dojo-blog/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+const App = () => {
+  
+  return (
+    <>
+      <div className="App">
+        <div className="contet">
+          <h1>App Component</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
