6d516fda9b2e4638f2671665514a2cd33775e99f
Faridah Salami <f.salami@outlook.com>
2026-03-13 11:10:43 +0000

    feat: Home and Navbar components created and global styling added to index.css

--- ReactModule/Udemy-Course/Ninja-tutorial/dojo-blog/src/App.jsx
+++ ReactModule/Udemy-Course/Ninja-tutorial/dojo-blog/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import Navbar from './components/Navbar/Navbar.jsx'
+import Home from './pages/Home/Home.jsx'
 
 const App = () => {
   
   return (
     <>
       <div className="App">
-        <div className="contet">
-          <h1>App Component</h1>
+        <Navbar />
+        <div className="content">
+          <Home/>
         </div>
       </div>
     </>
